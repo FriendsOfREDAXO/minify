1eabe703389f034af0f2dc8cab8b43e5b3401683
Thomas Kaegi <tk@meeloo.ch>
2016-08-12 13:10:47 +0200

    DB-Feld 'media' hinzugefügt

--- update.php
+++ update.php
@@ -1,5 +1,6 @@
 <?php
 	rex_sql_table::get(rex::getTable('minify_sets'))
+	->ensureColumn(new rex_sql_column('media', 'varchar(250)'))
 	->ensureColumn(new rex_sql_column('output', 'varchar(30)'))
 	->alter();
 ?>
