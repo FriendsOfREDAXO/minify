ae382affae347ec2931260eb77ab5c0ebe11a9e1
Thomas Kaegi <tk@meeloo.ch>
2016-09-15 14:58:58 +0200

    'media'-Feld entfernt; 'attributes'-Feld hinzugefügt; 'media'-Daten übernommen

--- update.php
+++ update.php
@@ -1,6 +1,14 @@
 <?php
 	rex_sql_table::get(rex::getTable('minify_sets'))
-	->ensureColumn(new rex_sql_column('media', 'varchar(250)'))
+	->ensureColumn(new rex_sql_column('attributes', 'text'))
 	->ensureColumn(new rex_sql_column('output', 'varchar(30)'))
+	->alter();
+	
+	$sql = rex_sql::factory();
+	$sql->setQuery('UPDATE `'.rex::getTablePrefix().'minify_sets` SET attributes = CONCAT("media=\"",media,"\"") WHERE `media` != ""');
+	unset($sql);
+	
+	rex_sql_table::get(rex::getTable('minify_sets'))
+	->removeColumn('media');
 	->alter();
 ?>
