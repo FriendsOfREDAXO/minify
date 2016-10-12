3166eb1eff236675b941907bb4fa12912d584c23
Thomas Kaegi <tk@meeloo.ch>
2016-10-12 13:24:43 +0200

    Sicherstellen, dass die Spalte 'media' existiert (Abwärtskomp.)

--- update.php
+++ update.php
@@ -1,5 +1,6 @@
 <?php
 	rex_sql_table::get(rex::getTable('minify_sets'))
+	->ensureColumn(new rex_sql_column('media', 'text'))
 	->ensureColumn(new rex_sql_column('attributes', 'text'))
 	->ensureColumn(new rex_sql_column('output', 'varchar(30)'))
 	->alter();
@@ -8,7 +9,8 @@
 	$sql->setQuery('UPDATE `'.rex::getTablePrefix().'minify_sets` SET attributes = CONCAT("media=\"",media,"\"") WHERE `media` != ""');
 	unset($sql);
 	
+	
 	rex_sql_table::get(rex::getTable('minify_sets'))
-	->removeColumn('media');
+	->removeColumn('media')
 	->alter();
 ?>
