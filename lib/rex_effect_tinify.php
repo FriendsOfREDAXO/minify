150f665f36d847862691400136269391b8674159
Thomas Kaegi <tk@walderwerber.ch>
2017-02-14 13:59:53 +0100

    Bugfix

--- lib/rex_effect_tinify.php
+++ lib/rex_effect_tinify.php
@@ -10,7 +10,22 @@
 				\Tinify\Tinify::setKey($key);
 				
 				$this->media->asImage();
-				$source = \Tinify\fromFile($this->media->getMediapath());
+				
+				//Start - get imageSource
+					ob_start();
+					if ($this->image['format'] == 'jpg' || $this->image['format'] == 'jpeg') {
+						$this->image['quality'] = rex_config::get('media_manager', 'jpg_quality', 80);
+						imagejpeg($this->image['src'], null, $this->image['quality']);
+					} elseif ($this->image['format'] == 'png') {
+						imagepng($this->image['src']);
+					} else {
+						return;
+					}
+					$src = ob_get_contents();
+					ob_end_clean();
+				//End - get imageSource
+				
+				$source = \Tinify\fromBuffer($src);
 				
 				$buffer = $source->toBuffer();
 				
