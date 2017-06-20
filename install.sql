8f309ced28529f3ead51042db8b3e6347ff2efa9
Thomas Kaegi <tk@walderwerber.ch>
2017-06-20 08:44:44 +0200

    Bugfix

--- install.sql
+++ install.sql
@@ -1,16 +1,11 @@
 CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%minify_sets` (
-  `id` int(11) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL DEFAULT '',
   `type` varchar(30) NOT NULL DEFAULT '',
   `minimize` enum('no','yes') NOT NULL DEFAULT 'no',
   `ignore_browsercache` enum('no','yes') NOT NULL DEFAULT 'no',
   `attributes` text NOT NULL DEFAULT '',
   `output` varchar(30) NOT NULL DEFAULT '',
-  `assets` text NOT NULL DEFAULT ''
+  `assets` text NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE `%TABLE_PREFIX%minify_sets`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `%TABLE_PREFIX%minify_sets`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
