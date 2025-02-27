61c4efb31107da32a2cc69e2cf59df197dc2d771
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-27 15:56:17 +0100

    Vendorupdate
    matthiasmullie/minify (1.3.71 => 1.3.73)
    
    cs

--- pages/config.php
+++ pages/config.php
@@ -16,11 +16,9 @@ if (rex_post('config-submit', 'boolean')) {
         rex_dir::create(rex_path::base(substr($post['pathcss'], 1)));
         rex_dir::create(rex_path::base(substr($post['pathjs'], 1)));
         $content .= rex_view::info($this->i18n('config_saved'));
-
     } catch (rex_exception $e) {
         $content .= rex_view::warning($this->i18n('config_error'));
     }
-
 }
 
 $content .= '<div class="rex-form">';
